27ba1019f2f6ea156f7e0f969e95070520fb9753
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-21 10:18:00 +0100

    Update icons.component.ts

--- src/app/icons/icons.component.ts
+++ src/app/icons/icons.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Reclamation } from 'app/classes/reclamation';
+import { ReclamationService } from 'app/services/reclamation.service';
 
 @Component({
   selector: 'app-icons',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icons.component.css']
 })
 export class IconsComponent implements OnInit {
+  grade:string;
+  reclamation:Reclamation;
+  reclamations:Reclamation[];
+  constructor(private reclamationService:ReclamationService) { }
+  
+  ngOnInit(): void {
+    this.reclamation=new Reclamation();
+    this.read();
 
-  constructor() { }
-
-  ngOnInit() {
+    this.grade=localStorage.getItem("grade");
   }
+  add()
+  {
+    this.reclamation.date=Date();
+    let rec=Object.assign({},this.reclamation);
 
+    this.reclamationService.create_NewReclamation(rec);
+    this.reclamation=new Reclamation();
+
+  }
+  read()
+  {
+    this.reclamationService.read_Reclamations().subscribe(data => {
+  
+      this.reclamations = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+         titre: e.payload.doc.data()["titre"],
+         description: e.payload.doc.data()["description"],
+         date: e.payload.doc.data()["date"],
+       
+  
+  
+  
+        };
+      });
+      console.log(this.reclamations);
+  
+    });
+  
+  
+  }
+  delete(id)
+  {
+    if(confirm("êtes vous sûre de vouloir supprimer? "))
+    {
+      this.reclamationService.delete_Reclamation(id);
+    }
+  }
+  
 }
