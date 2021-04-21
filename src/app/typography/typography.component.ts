632e071aab084f0c6758602e7e6e22beb925ae0b
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-21 10:18:57 +0100

    Update typography.component.ts

--- src/app/typography/typography.component.ts
+++ src/app/typography/typography.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Recommandation } from 'app/classes/recommandation';
+import { RecommandationService } from 'app/services/recommandation.service';
 
 @Component({
   selector: 'app-typography',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./typography.component.css']
 })
 export class TypographyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  grade:string;
+  recommandation:Recommandation;
+  recommandations:Recommandation[];
+  constructor(private recommandationService:RecommandationService) { }
+  
+  ngOnInit(): void {
+    this.read();
+    this.recommandation=new Recommandation();
+    this.grade=localStorage.getItem("grade");
   }
+  add()
+  {
+    let rec=Object.assign({},this.recommandation);
+    this.recommandationService.create_NewRecommandation(rec);
+    this.recommandation=new Recommandation();
 
+  }
+  read()
+  {
+    this.recommandationService.read_Recommandations().subscribe(data => {
+  
+      this.recommandations = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+         titre: e.payload.doc.data()["titre"],
+         texte: e.payload.doc.data()["texte"],
+       
+  
+  
+  
+        };
+      });
+      console.log(this.recommandations);
+  
+    });
+  
+  
+  }
+  delete(id)
+  {
+    if(confirm("êtes vous sûre de vouloir supprimer? "))
+    {
+      this.recommandationService.delete_Recommandation(id);
+    }
+  }
+  
 }
